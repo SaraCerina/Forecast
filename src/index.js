eefe3fd4206492b17fc1a1ef3baa36a93fc16bae
SaraCerina <cerinasara@gmail.com>
2024-02-19 20:30:35 +0100

    Injected additional current weather data
    Injected dynamic day of the week, time, weather condition, and humidity and wind speed values.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,69 @@
+function updateWeather(response) {
+  let currentTemperature = document.querySelector("#weather-value");
+  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+
+  let searchedCity = document.querySelector("#weather-city-name");
+  searchedCity.innerHTML = response.data.city;
+
+  let condition = document.querySelector("#weather-condition");
+  condition.innerHTML = response.data.condition.description;
+
+  let humidity = document.querySelector("#weather-humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeed = document.querySelector("#wind");
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let time = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  time.innerHTML = formatDate(date);
+}
+
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Satursday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+
+
+function searchCity(city) {
+  let apiKey = "efaac33t940b8a24381d0c380bbb4ddo";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+
+
 function handleSearchEvent(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let searchedCity = document.querySelector("#forecast-city-name");
-  searchedCity.innerHTML = searchFormInput.value;
+  searchCity(searchFormInput.value);
 }
+
+
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSearchEvent);
+
+searchCity("Barcelona");
+
